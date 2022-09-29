901a2bbebd4187bdbc7dae331d5838e56457ef96
Ahmed Attallah <ahmedattallah19010@gmail.com>
2022-09-29 16:59:52 +0200

    first commit

---
+++ components/icons/index.js
@@ -0,0 +1,4 @@
+export { default as AboutIcon } from "./AboutIcon";
+export { default as AddIcon } from "./AddIcon";
+export { default as UpdateIcon } from "./UpdateIcon";
+export { default as DeleteIcon } from "./DeleteIcon";
